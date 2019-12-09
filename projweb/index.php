a5465583422634efb2d7e6966d3fc51ab44cbea3
GBFELIX <47210907+GBFELIX@users.noreply.github.com>
2019-12-09 11:42:16 -0300

    Add files via upload

---
+++ projweb/index.php
@@ -0,0 +1,57 @@
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Info-Drugs</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css">
+    <script src="main.js"></script>
+    <link rel="shortcut icon" href="imgs/a.png">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous">
+</head>
+<header>
+    
+    <h2>
+        <u></u>
+    </h2>
+    <p>
+        <div class="foto">
+            
+            <img src="imgs/logo.png" style="width: 12rem; height: 12rem">
+
+        </div>
+</header>
+
+<body>
+        
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link active" href="index.php">
+                <h4>Home</h4>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="form.php">
+                <h4>Preços</h4>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="local.html">
+                <h4>Farmácias Proximas</h4>
+            </a>
+        </li>
+    </ul>
+    <figure>
+        <img src="" alt="">
+    </figure>
+</body>
+<footer>
+     
+    <div class="foto">
+            <img src="imgs/remedio.png">
+        </div> 
+</footer>
+</html>
