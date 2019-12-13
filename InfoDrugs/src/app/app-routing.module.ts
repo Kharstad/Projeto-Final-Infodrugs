bc07cfdbcd3e21a8cacdd3a0c60dfe5d9421ec91
Enos Fernandes <kyrotth@gmail.com>
2019-12-13 11:10:53 -0300

    ...

--- InfoDrugs/src/app/app-routing.module.ts
+++ InfoDrugs/src/app/app-routing.module.ts
@@ -51,6 +51,11 @@ const routes: Routes = [
     path: 'perfil-usuario',
     loadChildren: () => import('./pages/perfil-usuario/perfil-usuario.module').then( m => m.PerfilUsuarioPageModule)
   },
+  {
+    path: 'perfil-usuario',
+    loadChildren: () => import('./pages/perfil-usuario/perfil-usuario.module').then( m => m.PerfilUsuarioPageModule)
+  },
+
 
 
 
