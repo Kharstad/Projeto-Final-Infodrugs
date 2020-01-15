cb14b734cce88b1e60bf088647b2324ac082fe3d
Enos Fernandes <kyrotth@gmail.com>
2020-01-15 11:12:03 -0300

    15/01

--- InfoDrugs/src/app/pages/localizar-farmacia/localizar-farmacia.page.ts
+++ InfoDrugs/src/app/pages/localizar-farmacia/localizar-farmacia.page.ts
@@ -3,6 +3,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Platform } from '@ionic/angular';
 import { GoogleMaps, GoogleMap, GoogleMapsEvent, Marker, MarkerCluster, MyLocation, LocationService, LatLng } from '@ionic-native/google-maps';
 import { Farmacia } from '../../model/farmacia';
+import { getLocaleCurrencyName } from '@angular/common';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
 
 @Component({
   selector: 'app-localizar-farmacia',
@@ -12,70 +15,12 @@ import { Farmacia } from '../../model/farmacia';
 export class LocalizarFarmaciaPage implements OnInit {
   protected map: GoogleMap;
   constructor(
-    protected farmacia: Farmacia = new Farmacia,
     protected router: Router,
     protected activatedRoute: ActivatedRoute,
     private geolocation: Geolocation,
     private platform: Platform,
   ) { }
 
-  async ngOnInit() {
-    await this.loadMap();
-  }
-  loadMap() {
-    this.map = GoogleMaps.create('map_canvas', {
-      'camera': {
-        'target': {
-          "lat": this.farmacia.lat,
-          "lng": this.farmacia.lng,
-        },
-        'zoom': 18
-      }
-    });
-    //this.addCluster(this.dummyData());
-    this.minhaLocalizacao();
-  }
-  minhaLocalizacao() {
-    LocationService.getMyLocation().then(
-      (myLocation: MyLocation) => {
-        this.map.setOptions({
-          camera: {
-            target: myLocation.latLng
-          }
-        })
-        //marcadores
-        let marker: Marker = this.map.addMarkerSync({
-          position: {
-            lat: myLocation.latLng.lat,
-            lng: myLocation.latLng.lng
-          },
-          icon: "#00ff00",
-          title: this.farmacia.nome,
-          snippet: this.farmacia.nickname,
-        })
-        //adicionar eventos ao mapa
-        this.map.on(GoogleMapsEvent.MARKER_CLICK).subscribe(
-          res => {
-            marker.setTitle(this.farmacia.nome)
-            marker.setSnippet(this.farmacia.nickname)
-            marker.showInfoWindow()
-          }
-        )
-        //colocar pontos extras
-        this.map.on(GoogleMapsEvent.MAP_CLICK).subscribe(
-          res => {
-            console.log(res)
-            // this.map.addMarker({
-            //   position: {
-            //     lat: res[0].lat,
-            //     lng: res[0].lng
-            //   }
-            // })
-            marker.setPosition(res[0])
-          }
-        )
-      }
-    );
-  }
-}
+  async ngOnInit() {}
 
+}
